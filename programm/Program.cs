6c30117b1b3af80c5ef57d8be7761f59f586c9bf
Nikita Overchuk <nik.overchuk.97@mail.ru>
2022-09-19 13:12:37 +0300

    решение

---
+++ programm/Program.cs
@@ -0,0 +1,21 @@
+﻿int arrayLenght = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+string[] array = new string[arrayLenght];
+int k = 3;
+while (index < arrayLenght)
+{
+    array[index] = Console.ReadLine();
+    index = index + 1;
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.Write("[");
+index = 0;
+while (index < arrayLenght)
+{
+    if (array[index].Length <= k)
+    {
+        Console.Write(array[index]);
+    }
+    index = index + 1;
+}
+Console.WriteLine("]");
